486274ff74dcb73a57caf0ddb692a70f46c06031
Sanjay_Baladara <baladarasanjay@gmail.com>
2023-08-20 09:52:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Card from "./Card";
+
+function App() {
+  return (
+    <>
+      <Card />
+      <div className="card">Hii</div>
+      <p className="read-the-docs">Good...!</p>
+    </>
+  );
+}
+
+export default App;
